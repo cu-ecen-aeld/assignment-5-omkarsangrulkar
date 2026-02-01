1d6b85d84cdb51734974e6136578242a65e0226d
omkarsangrulkar <omkarsangrulkar14@gmail.com>
2026-02-01 10:03:22 -0700

    test part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,10 +10,29 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    // Sleep for wait_to_obtain_ms before attempting to obtain the mutex
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    // Obtain the mutex
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to lock mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // Hold the mutex for wait_to_release_ms
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    // Release the mutex
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to unlock mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +47,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
 
+    struct thread_data* td = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (td == NULL) {
+        ERROR_LOG("Failed to allocate thread_data");
+        return false;
+    }
+
+    td->mutex                = mutex;
+    td->wait_to_obtain_ms    = wait_to_obtain_ms;
+    td->wait_to_release_ms   = wait_to_release_ms;
+    td->thread_complete_success = false;
+
+    if (pthread_create(thread, NULL, threadfunc, td) != 0) {
+        ERROR_LOG("Failed to create thread");
+        free(td);
+        return false;
+    }
+
+    return true;
+}
